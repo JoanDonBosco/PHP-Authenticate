764f23d5f30accf81cffc151b7190f67a257929e
JoanDonBosco <jalcantarap36@gmail.com>
2022-03-01 18:35:11 +0100

    master

--- Si-MYSQL/add.php
+++ Si-MYSQL/add.php
@@ -1,21 +1,30 @@
 <?php
-  
   // Importem el fitxer database.php i en conseqüencia les seves variables
   require "database.php";
-
+  // Crem variables de error per gestionar-los
+  $error = null;
   // Si rebem un per les capçaleres un metode POST entrara dins del POST
   if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    //Variables que rebem per POST
-    $nom = $_POST["name"];
-    $num_phone = $_POST["num"];
-    // Preparem una syntaxis SQL per insertar les dades que s'han passat per el POST
-    $statement = $conn->prepare("INSERT INTO contacts (name, num_phone) VALUES ('$nom', '$num_phone')");
-    // Executem la syntaxis anteriorment preparada
-    $statement->execute();
-    // Enviem al index.php
-    header("Location: index.php");
-}
-
+      // Si el parametres name i num enviats per post estan buits retornem el misstage de error
+      if (empty($_POST["name"]) || empty($_POST["num"])) {
+        $error = "Sisplau entri un nom i telèfon al contacte!";
+      } else if (strlen($_POST["num"]) < 6) {
+        $error = "Entra un número més gran o igual que 6!"; 
+      }else {
+        //Variables que rebem per POST
+        $nom = $_POST["name"];
+        $num_phone = $_POST["num"];
+        // Preparem una syntaxis SQL amb valors incognita per prevenir un SQL Inyection!
+        $statement = $conn->prepare("INSERT INTO contacts (name, num_phone) VALUES (:name, :num)");
+        // Comprobem que els valors pasats per POST no son un atac a la base de dades i canviem les variables :name i :num per les que ens pasen per el POST el usuari
+        $statement->bindParam(":name", $_POST["name"]);
+        $statement->bindParam(":num", $_POST["num"]);
+        // Executem la syntaxis anteriorment preparada
+        $statement->execute();
+        // Enviem al index.php
+        header("Location: index.php");
+      }
+  }
 ?>
 
 <!DOCTYPE html>
@@ -79,12 +88,16 @@
           <div class="card">
             <div class="card-header">Add New Contact</div>
             <div class="card-body">
+              <!-- If per mostra els errors en el cas que la variable tinqgui un error assignat -->
+              <?php if ($error != null): ?>
+                <p class="text-danger"><?= $error ?></p>
+              <?php endif ?>
               <form method="POST" action="add.php">
                 <div class="mb-3 row">
                   <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
     
                   <div class="col-md-6">
-                    <input id="name" type="text" class="form-control" name="name" required autocomplete="name" autofocus>
+                    <input id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
                   </div>
                 </div>
     
@@ -92,7 +105,7 @@
                   <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
     
                   <div class="col-md-6">
-                    <input id="phone_number" type="tel" class="form-control" name="num" required autocomplete="phone_number" autofocus>
+                    <input id="phone_number" type="tel" class="form-control" name="num" autocomplete="phone_number" autofocus>
                   </div>
                 </div>
     
