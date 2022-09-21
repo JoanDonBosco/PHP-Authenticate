0ac0e57182b8a68c43f45204e64c461b11da04c4
Joan Don Bosco <isaac.alaezvidal@gmail.com>
2022-09-21 14:57:28 +0200

    Update database.php

--- Si-MYSQL/database.php
+++ Si-MYSQL/database.php
@@ -1,17 +1,17 @@
 <?php 
 // Parametres connexió base de dades
     // IP on es troba MySQL
-    $HOST="109.111.117.135";
+    $HOST="Public IP";
     // La base de dades en la que volem treballar en aquest cas
     $DATABASE="contacts_app";
     // Usuari que tinguem al MySQL per authrnyicarnos
     $USER="user";
     // Contrasenya del Usuari MySQL
-    $PASS="Roxi2003";
+    $PASS="password";
     // Port per on es fara la connexió a la base de dades per Defecte MySQL utilitza 3306 pero en el cas que segui un altre el canviem
     $PORT="10012";
     // String o cadena per dur a terme la connexió a la base de dades en el cas que no utilitzem el port default
-    $dsn='mysql:dbname=contacts_app;host=109.111.117.135;port=10012';
+    $dsn='mysql:dbname=contacts_app;host=PublicIP;port=10012';
 
     // Connexió amb port Diferent i utilitzant DSN
     try {
